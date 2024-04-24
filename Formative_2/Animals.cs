3323af1aaa88110e7a1450e24221d20cc291a1dc
DeiEquinox <144390923+DeusEquinox@users.noreply.github.com>
2024-04-24 23:17:25 +0200

    Add project files.

---
+++ Formative_2/Animals.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Formative_2
+{
+
+	interface IFeedable
+	{
+		void Eat(List<string> Foods, string food);
+	}
+
+	interface IMovable
+	{
+		void Move(string? location);
+	}
+	public class Animals : IMovable, IFeedable
+	{
+		public string? Name;
+		public int Age;
+		public string? Type;
+		public string? Kind;
+		public string? CurrentLocation;
+		public List<string> Location = ["Enclosure", "Feeding area", "Sleeping area"];
+		public List<string> Food = [];
+		
+
+		public Animals(string? name, int age)
+		{
+			this.Name = name;
+			this.Age = age;
+
+			//Assigning a food list based on what kind of animal diet it follows
+			if (Type == "Carnivore")
+			{
+				List<string> Meat = ["Chicken", "Beef", "Pork", "Lamb"];
+				Food.AddRange(Meat);
+			}
+			else if (Type == "Herbivore")
+			{
+				List<string> FruitAndVeg = ["Berries", "Apple", "Oranges", "Bananas"];
+				Food.AddRange(FruitAndVeg);
+			}
+			else if (Type == "Omnivore")
+			{
+				List<string> AllFood = ["Chicken", "Beef", "Pork", "Lamb", "Berries", "Apple", "Oranges", "Bananas"];
+				Food.AddRange(AllFood);
+			}
+		}
+
+		public virtual void Display()
+		{
+			Console.WriteLine(Name + " the " + Kind + " is a " + Type + " and is " + Age + " years old.");
+		}
+
+		public void Eat(List<string> Foods, string food)
+		{
+			for (int i = 0; i < Foods.Count; i++)
+			{
+				if (Foods[i] == food)
+				{
+					Console.WriteLine(Name + " is being fed " + food);
+					return;
+				}
+			}
+			Console.WriteLine("We dont have that here. Please try again.");
+		}
+
+		public void Eat(List<string> Foods, int food)
+		{
+			while (food > Foods.Count)
+			{
+				Console.Write("Please choose one of the options from the list: ");
+				try
+				{
+					food = Convert.ToInt32(Console.ReadLine());
+				}
+				catch (Exception)
+				{
+					Console.WriteLine("Please enter number");
+				}
+			}
+			Console.WriteLine(Name + " is being fed " + Foods[food - 1]);
+		}
+
+		public void Sleep(List<Animals> AL, int i)
+		{
+			AL[i].CurrentLocation = "Sleeping area";
+			Console.WriteLine(AL[i].Name + " is sleeping");
+		}
+
+		public virtual void Speak()
+		{
+			Console.WriteLine(Name + " makes animal noises");
+		}
+
+		public void Move(string? location)
+		{
+			if (Location.Contains(location))
+			{
+				CurrentLocation = location;
+				Console.WriteLine("The animal is now in the " + CurrentLocation);
+			}
+			else
+			{
+				Console.WriteLine("Location not found. Please try again.");
+			}
+		}
+	}
+
+	class Lion : Animals
+	{
+		public Lion(string? name, int age) : base(name, age)
+		{
+			this.Kind = "Lion";
+			this.Type = "Carnivore";
+
+			List<string> Meat = ["Chicken", "Beef", "Pork", "Lamb"];
+			Food.AddRange(Meat);
+		}
+
+		public override void Display()
+		{
+			Console.WriteLine(Name + " the Lion is a " + Type + " and is " + Age + " years old.");
+		}
+
+		public override void Speak()
+		{
+			Console.WriteLine("Roar");
+		}
+	}
+
+	class Parrot : Animals
+	{
+		public Parrot(string? name, int age) : base(name, age)
+		{
+			this.Kind = "Parrot";
+			this.Type = "Herbivore";
+
+			List<string> FruitAndVeg = ["Berries", "Apple", "Oranges", "Bananas"];
+			Food.AddRange(FruitAndVeg);
+		}
+
+		public override void Display()
+		{
+			Console.WriteLine(Name + " the Parrot is a " + Type + " and is " + Age + " years old.");
+		}
+
+		public override void Speak()
+		{
+			Console.WriteLine("Squawk");
+		}
+	}
+
+	class Turtle : Animals
+	{
+		public Turtle(string? name, int age) : base(name, age)
+		{
+			this.Kind = "Turtle";
+			this.Type = "Omnivore";
+
+			List<string> AllFood = ["Chicken", "Beef", "Pork", "Lamb", "Berries", "Apple", "Oranges", "Bananas"];
+			Food.AddRange(AllFood);
+		}
+
+		public override void Display()
+		{
+			Console.WriteLine(Name + " the Turtle is a " + Type + " and is " + Age + " years old.");
+		}
+
+		public override void Speak()
+		{
+			Console.WriteLine($"\n{Name} Croaks\n");
+		}
+	}
+}
---
+++ Formative_2/Program.cs
@@ -0,0 +1,225 @@
+﻿using Formative_2;
+
+class Program
+{
+	public static void Main()
+	{
+	//Creating 3 animals and placing them in a list
+		List<Animals> AnimalList = new();
+
+		Parrot parrot = new("Greg", 25);
+		Lion lion = new("Mufasa", 10);
+		Turtle turtle = new("Bob", 100);
+		
+		AnimalList.Add(parrot);
+		AnimalList.Add(lion);
+		AnimalList.Add(turtle);
+
+		Console.WriteLine("Welcome to the Virtual Zoo");
+		DisplayMenu(AnimalList);
+	}
+
+	public static void DisplayMenu(List<Animals> AL)
+	{
+		//Display the main menu and trigger a function based on user input
+		Console.WriteLine(""""
+			
+			1. Display the animals 
+			2. Feed the animals
+			3. Move the animals
+			4. Make the animals speak
+			5. Put the animals to sleep
+			6. Add an animal
+			7. Exit
+			"""");
+
+		Console.Write("Enter your choice: ");
+		string? choice = Console.ReadLine();
+
+		switch (choice)
+		{
+			case "1":
+				DisplayAnimals(AL);
+				break;
+			case "2":
+				FeedAnimals(AL);
+				break;
+			case "3":
+				MoveAnimals(AL);
+				break;
+			case "4":
+				MakeAnimalsSpeak(AL);
+				break;
+			case "5":
+				PutAnimalsToSleep(AL);
+				break;
+			case "6":
+				AddAnimal(AL);
+				break;
+			case "7":
+				Environment.Exit(0);
+				break;
+			default:
+				Console.WriteLine("Invalid choice. Please try again.");
+				break;
+		}
+
+		DisplayMenu(AL);
+	}
+
+	public static void DisplayAnimals(List<Animals> AL)
+	{
+		//Display the animals proprties
+		Console.WriteLine("\nList of all the animals");
+		for (int i = 0; i < AL.Count; i++)
+		{
+			AL[i].Display();
+		}
+	}
+
+	public static void FeedAnimals(List<Animals> AL)
+	{
+		//Display animal properties
+		DisplayAnimals(AL);
+		Console.Write("Enter the NAME (not type) of the animal you want to feed: ");
+		string? name = Console.ReadLine();
+
+		//Checking for the user enered name within the list of animals
+		for (int i = 0; i < AL.Count; i++)
+		{
+			if (AL[i].Name == name)
+			{
+				ChoiceCheck(AL, i);
+				return;
+			}
+		}
+
+		Console.WriteLine("Animal not found. Please try again.");
+	}
+
+	public static void ChoiceCheck(List<Animals> AL, int index)
+	{
+		//Showing the user what food they can feed the animal and letting them choose
+		for (int i = 0; i < AL[index].Food.Count; i++)
+		{
+			Console.WriteLine(i + 1 + ". " + AL[index].Food[i]);
+		}
+		Console.Write($"What do you want to feed {AL[index].Name}: ");
+		string? Food = Console.ReadLine();
+		int FoodChoice;
+
+		//Using overloading to allow the user to choose by entering the name of the food or the number on the list
+		try
+		{
+			FoodChoice = Convert.ToInt32(Food);
+			AL[index].Eat(AL[index].Food, FoodChoice);
+		}
+		catch (Exception)
+		{
+			AL[index].Eat(AL[index].Food, Food);
+		}
+	}
+
+	public static void MoveAnimals(List<Animals> AL)
+	{
+		// Asking the user what animal they  want to move and where
+		Console.Write("Enter the name of the animal you want to move: ");
+		string? name = Console.ReadLine();
+
+		Console.Write("Enter the location you want to move the animal to (Enclosure, Feeding area, Sleeping area): ");
+		string? location = Console.ReadLine();
+
+		for (int i = 0; i < AL.Count; i++)
+		{
+			if (AL[i].Name == name)
+			{
+				AL[i].Move(location);
+				return;
+			}
+		}
+
+		Console.WriteLine("Animal not found. Please try again.");
+	}
+
+	public static void MakeAnimalsSpeak(List<Animals> AL)
+	{
+
+		Console.Write("Enter the name of the animal you want to hear the sound off: ");
+		string? name = Console.ReadLine();
+
+		//Making the animal speak based on which animal the user chose
+		for (int i = 0; i < AL.Count; i++)
+		{
+			if (AL[i].Name == name)
+			{
+				AL[i].Speak();
+				return;
+			}
+		}
+		Console.WriteLine("Animal not found. Please try again.");
+	}
+
+	public static void PutAnimalsToSleep(List<Animals> AL)
+	{
+		Console.Write("Enter the name of the animal you want make sleep: ");
+		string? name = Console.ReadLine();
+
+		//Making the animal sleep based on which one the user chose
+		for (int i = 0; i < AL.Count; i++)
+		{
+			if (AL[i].Name == name)
+			{
+				AL[i].Sleep(AL, i);
+				return;
+			}
+		}
+		Console.WriteLine("Animal not found. Please try again.");
+	}
+
+	public static void AddAnimal(List<Animals> AL)
+	{
+		//Asking the user for the properties of the animals with error cheching
+		Console.Write("Enter the name of the animal: ");
+		string? name = Console.ReadLine();
+
+		Console.Write("Enter the type of the animal (lion, parrot, turtle): ");
+		string? type = Console.ReadLine();
+
+		while (type.ToLower() is not "lion" and not "parrot" and not "turtle")
+		{
+			Console.Write("Please enter a valid type: ");
+			type = Console.ReadLine();
+		}
+
+		Console.Write("Enter the age of the animal: ");
+		int age = 0;
+		while (age == 0)
+		{
+			try
+			{
+				age = Convert.ToInt32(Console.ReadLine());
+			}
+			catch (Exception)
+			{
+				Console.Write("Please enter a number: ");
+			}
+		}
+
+		//Creating the animal based on the users properties
+		if (type.Equals("Lion", StringComparison.CurrentCultureIgnoreCase))
+		{
+			AL.Add(new Lion(name, age));
+			Console.WriteLine($"{name} the Lion has been added to the Zoo");
+		}
+		else if (type.Equals("Parrot", StringComparison.CurrentCultureIgnoreCase))
+		{
+			AL.Add(new Parrot(name, age));
+			Console.WriteLine($"{name} the Parrot has been added to the Zoo");
+		}
+		else if (type.Equals("Turtle", StringComparison.CurrentCultureIgnoreCase))
+		{
+			AL.Add(new Turtle(name, age));
+			Console.WriteLine($"{name} the Turtle has been added to the Zoo");
+		}
+	}	
+}
